cebc45957428fa70505e054f6df0097c2320a8d1
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-05-24 17:42:26 +0530

    Rows Input

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,23 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        FindTheWords
-      </h1>
-      <h2 class="subtitle">
-        My transcendent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      <h1 class="title">FindTheWords</h1>
+    </div>
+    <div id="app">
+      <v-app id="inspire">
+        <v-container fluid>
+          <v-layout row>
+            <v-flex xs8>
+              <v-text-field label="Number of Rows" value="10.0"></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-app>
     </div>
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
 .container {
@@ -45,8 +30,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -54,14 +39,13 @@ export default {
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.inputs {
+  font-family: "Source Sans Pro", "Arial";
+  display: block;
+  font-weight: 50;
+  font-size: 16px;
+  color: black;
 }
-
 .links {
   padding-top: 15px;
 }
